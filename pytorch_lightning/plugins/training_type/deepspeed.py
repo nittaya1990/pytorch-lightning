9d2c17ecafca0c6468934d73470eff4f1dce74fd
Justus Schock <justus.schock@posteo.de>
2021-09-08 20:00:34 +0200

    deepspeed

--- pytorch_lightning/plugins/training_type/deepspeed.py
+++ pytorch_lightning/plugins/training_type/deepspeed.py
@@ -18,7 +18,7 @@ import os
 import platform
 from collections import OrderedDict
 from pathlib import Path
-from typing import Any, Callable, Dict, Genrator, List, Mapping, Optional, Tuple, Union
+from typing import Any, Callable, Dict, Generator, List, Mapping, Optional, Tuple, Union
 
 import torch
 from torch.optim import Optimizer
@@ -826,4 +826,4 @@ class DeepSpeedPlugin(DDPPlugin):
         return self.model(*args, **kwargs)
 
     def predict_step(self, *args, **kwargs):
-        return self.model(*args, **kwargs)e
+        return self.model(*args, **kwargs)
